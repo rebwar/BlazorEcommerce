b59c3f942423194c864a1f2f821da0230abae253
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2022-01-16 23:03:33 +0330

    Add Repository Pattern to the Client Side and refactoring code

---
+++ BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorEcommerce.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        public List<Product> Products { get; set; }
+        Task GetProducts();
+
+    }
+}
---
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorEcommerce.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _http;
+
+        public List<Product> Products { get; set; } = new List<Product>();
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+        public async Task GetProducts()
+        {
+            var response = await _http.GetFromJsonAsync<ServiceResponce<List<Product>>>("api/product");
+            Products = response.Data;           
+        }
+    }
+}

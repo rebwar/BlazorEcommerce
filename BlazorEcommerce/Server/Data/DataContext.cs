332faf1d4f3d9cb65b7e0cbaa700a08f347f69bf
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2022-01-16 21:05:55 +0330

    Add DbContext to DI and create database and add migration

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using BlazorEcommerce.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
